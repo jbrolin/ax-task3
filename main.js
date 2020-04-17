14beb7ded54547cc0f92bd578a682a5f264ce737
jbrolin <brolin.jonas@gmail.com>
2020-04-17 14:12:13 +0200

    fix minor

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
 // task3
 
-const sortAnagramString = (inputString) => {
-  let tempArray=inputString.toLowerCase().split("")
-  tempArray.sort();
-  return tempArray.join("");
-}
-
 const run3 = () => {
   const str1=document.getElementById("String1").value;
   const str2=document.getElementById("String2").value;
@@ -18,4 +12,8 @@ console.log(output1 + "==" + output2 + ": " + (output1===output2));
 document.getElementById("outputBool").innerHTML = (output1===output2);
 }
 
-
+const sortAnagramString = (inputString) => {
+  let tempArray=inputString.toLowerCase().split("")
+  tempArray.sort();
+  return tempArray.join("");
+}
